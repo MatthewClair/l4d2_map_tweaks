f30518681c42a7a40c0fa287f1f9e8503c112ef3
Matthew Clair <Canada.Rox.Gh@gmail.com>
2013-08-11 12:23:22 -0400

    Add another property to include

--- convert_to_stripper.sh
+++ convert_to_stripper.sh
@@ -3,8 +3,7 @@
 echo "add:" > "${1%%.*}".cfg
 
 tr -d '\r' < $1 |\
-	awk '/^{\r?$/ { foundStart = 1; goodBlock = 0; foundEnd = 0; includeLine = 0 } /^}\r?$/ { foundEnd = 1 } /nfcrx_tweaks/ { if (foundStart == 1) { goodBlock = 1 }} /origin/ { includeLine = 1 } /solid/ { includeLine = 1 } /angles/ { includeLine = 1 } /model/ { includeLine = 1 } /classname/ { includeLine = 1 } /targetname/ { includeLine = 1 } /parentname/ { includeLine = 1 } foundStart { if (includeLine == 1) { s = s"\n"$0; includeLine = 0; } } foundEnd { if (goodBlock == 1) { print "{"s"\n}"; goodBlock = 0; foundStart = 0; foundEnd = 0 }; s = "" }' \
-	| sed -e 's/parentname/targetname/' >> "${1%%.*}".cfg
+	awk '/^{\r?$/ { foundStart = 1; goodBlock = 0; foundEnd = 0; includeLine = 0 } /^}\r?$/ { foundEnd = 1 } /nf_props/ { if (foundStart == 1) { goodBlock = 1 }} /origin/ { includeLine = 1 } /solid/ { includeLine = 1 } /angles/ { includeLine = 1 } /model/ { includeLine = 1 } /classname/ { includeLine = 1 } /targetname/ { includeLine = 1 } /rendercolor/ { includeLine = 1 } foundStart { if (includeLine == 1) { s = s"\n"$0; includeLine = 0; } } foundEnd { if (goodBlock == 1) { print "{"s"\n}"; goodBlock = 0; foundStart = 0; foundEnd = 0 }; s = "" }' >> "${1%%.*}".cfg
 
 exit
 
@@ -12,14 +11,14 @@ exit
 #!/bin/awk -f
 /^{\r?$/ { foundStart = 1; goodBlock = 0; foundEnd = 0; includeLine = 0 }
 	/^}\r?$/ { foundEnd = 1 }
-	/nfcrx_tweaks/ { if (foundStart == 1) { goodBlock = 1 }}
+	/nf_props/ { if (foundStart == 1) { goodBlock = 1 }}
 	/origin/ { includeLine = 1 }
 	/solid/ { includeLine = 1 }
 	/angles/ { includeLine = 1 }
 	/model/ { includeLine = 1 }
 	/classname/ { includeLine = 1 }
 	/targetname/ { includeLine = 1 }
-	/parentname/ { includeLine = 1 }
+	/rendercolor/ { includeLine = 1 }
 	foundStart { if (includeLine == 1) { s = s"\n"$0; includeLine = 0; } }
 	foundEnd { if (goodBlock == 1) { print "{"s"\n}"; goodBlock = 0; foundStart = 0; foundEnd = 0 }; s = "" }
 
