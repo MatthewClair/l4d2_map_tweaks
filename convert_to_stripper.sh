8a03f9a0b72b95db25308b28618783c4bb86556d
Matthew Clair <Canada.Rox.Gh@gmail.com>
2013-08-28 16:18:19 -0400

    add disable shadow option

--- convert_to_stripper.sh
+++ convert_to_stripper.sh
@@ -3,7 +3,7 @@
 echo "add:" > "${1%%.*}".cfg
 
 tr -d '\r' < $1 |\
-	awk '/^{\r?$/ { foundStart = 1; goodBlock = 0; foundEnd = 0; includeLine = 0 } /^}\r?$/ { foundEnd = 1 } /nf_props/ { if (foundStart == 1) { goodBlock = 1 }} /origin/ { includeLine = 1 } /solid/ { includeLine = 1 } /angles/ { includeLine = 1 } /model/ { includeLine = 1 } /classname/ { includeLine = 1 } /targetname/ { includeLine = 1 } /rendercolor/ { includeLine = 1 } foundStart { if (includeLine == 1) { s = s"\n"$0; includeLine = 0; } } foundEnd { if (goodBlock == 1) { print "{"s"\n}"; goodBlock = 0; foundStart = 0; foundEnd = 0 }; s = "" }' >> "${1%%.*}".cfg
+	awk '/^{\r?$/ { foundStart = 1; goodBlock = 0; foundEnd = 0; includeLine = 0 } /^}\r?$/ { foundEnd = 1 } /nf_props/ { if (foundStart == 1) { goodBlock = 1 }} /origin/ { includeLine = 1 } /solid/ { includeLine = 1 } /angles/ { includeLine = 1 } /model/ { includeLine = 1 } /classname/ { includeLine = 1 } /targetname/ { includeLine = 1 } /rendercolor/ { includeLine = 1 } /disableshadows/ { includeLine = 1 } foundStart { if (includeLine == 1) { s = s"\n"$0; includeLine = 0; } } foundEnd { if (goodBlock == 1) { print "{"s"\n}"; goodBlock = 0; foundStart = 0; foundEnd = 0 }; s = "" }' >> "${1%%.*}".cfg
 
 exit
 
@@ -19,6 +19,7 @@ exit
 	/classname/ { includeLine = 1 }
 	/targetname/ { includeLine = 1 }
 	/rendercolor/ { includeLine = 1 }
+	/disableshadows/ { includeLine = 1 }
 	foundStart { if (includeLine == 1) { s = s"\n"$0; includeLine = 0; } }
 	foundEnd { if (goodBlock == 1) { print "{"s"\n}"; goodBlock = 0; foundStart = 0; foundEnd = 0 }; s = "" }
 
